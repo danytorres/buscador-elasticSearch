94e7ad9b931d1a494df8220bd8550389f126f10a
Daniel Torres <danytorresbmx@gmail.com>
2021-01-05 13:09:07 -0600

    pruebas para deploy

---
+++ elasticSearch/views.py
@@ -0,0 +1,68 @@
+from django.http import HttpResponse
+import datetime
+from django.template import Template, Context
+from django.template import loader
+from django.shortcuts import render
+from gestionPedidos.models import Articulos
+
+def saludo(request): #primera vista
+
+    nombre = "Juan"
+    apellido = "Diaz"
+    ahora = datetime.datetime.now()
+    lista = ["php","django","html","elasticsearch"]
+    # lista = []
+
+    # doc_externo = open("/home/danytorres/buscador-elasticSearch/elasticSearch/elasticSearch/plantillas/miplantilla.html")
+    # plt = Template(doc_externo.read())
+    # doc_externo.close()
+
+    # doc_externo = loader.get_template('miplantilla.html') > carga la plantilla, se tiene que especificar la carpeta donde estan las plantillas en el documento settings.py en la seccion templates > DIR 
+
+    ctx = {"nombre_persona":nombre, "apellido_persona":apellido, "tiempo":ahora, "temas":lista} # contexto de la plantilla, de aqui se trae las variables que necesita la plantilla para mostrar su informacion 
+    # documento = doc_externo.render(ctx) > Renderiza la plantilla con el contexto dado
+
+    return render(request, "miplantilla.html", ctx)
+
+def despedida(request):
+    return HttpResponse("Hasta luego alumnos django")
+
+def dameFecha(request):
+    fecha_actual = datetime.datetime.now()
+    documento = """<html>
+    <body>
+    <h1>Fecha y hora actuales %s</h1>
+    </body>
+    </html>""" % fecha_actual
+
+    return HttpResponse(documento)
+
+def calculaEdad(request, edad, agno):
+    #edadActual = 18 
+    periodo = agno - 2020
+    edadFutura = edad + periodo
+    documento = "<html><body><h2>en el año %s tendras %s años</h2></body></html>" %(agno, edadFutura)
+    return HttpResponse(documento)
+
+def bootstrap(request):
+    # doc_externo = loader.get_template('principal.html')
+    # documento = doc_externo.render()
+    return render(request, "principal.html")
+
+def buscar(request):
+
+    if request.GET["prd"]:
+        # mensaje="Articulo buscado: %r"%request.GET["prd"]
+        producto = request.GET["prd"]
+
+        if len(producto)>20:
+            mensaje="Texto de busqueda demasiado largo"
+        else:
+
+            articulos = Articulos.objects.filter(nombre__icontains=producto)
+
+            return render(request, "resultado_busqueda.html", {"articulos":articulos, "query":producto})
+        
+    else:
+        mensaje = "No has introducido un valor"
+    return HttpResponse(mensaje)
